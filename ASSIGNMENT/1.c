d28787908b26506431b419a62b88d6ca5c9a0d3e
Sohini <145785887+sohini-adamas@users.noreply.github.com>
2023-09-28 21:19:44 +0530

    2ND COMMIT

--- ASSIGNMENT/1.c
+++ ASSIGNMENT/1.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int a,pro=1,c;
+    a=1;
+    printf("Enter Your Number\n");
+    scanf("%d",&c);
+    while(a<=c){
+        pro=pro*a;
+        a++;
+    }
+    printf("Factorial of %d is %d : ",c,pro);
 }
