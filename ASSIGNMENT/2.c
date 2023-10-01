529c18d220fc777a79ab535e385f04116d96e45b
Sohini <145785887+sohini-adamas@users.noreply.github.com>
2023-10-01 12:03:58 +0530

    3RD COMMIT

--- ASSIGNMENT/2.c
+++ ASSIGNMENT/2.c
@@ -2,5 +2,13 @@
 // Created by USER on 9
 #include <stdio.h>
 int main() {
-    printf("Hello, World!\n");
+    int n;
+    printf("enter number of lines you want to print :\n");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
 }
