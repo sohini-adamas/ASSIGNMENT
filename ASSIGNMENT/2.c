4876f40ea1ca8ee87ac4be2bee5a0593dffcdb28
Sohini <145785887+sohini-adamas@users.noreply.github.com>
2023-10-01 13:18:34 +0530

    4TH COMMIT

--- ASSIGNMENT/2.c
+++ ASSIGNMENT/2.c
@@ -5,7 +5,7 @@ int main() {
     int n;
     printf("enter number of lines you want to print :\n");
     scanf("%d",&n);
-    for(int i=1;i<=n;i++) {
+    for(int i=1;i<=n;i++){
         for (int j = 1; j <= i; j++) {
             printf("%d", j);
         }
