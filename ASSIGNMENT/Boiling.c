e0412b4e64c58398b29dd757225ac771973a77a6
Sohini <145785887+sohini-adamas@users.noreply.github.com>
2023-10-09 00:58:13 +0530

    1ST COMMIT

---
+++ ASSIGNMENT/Boiling.c
@@ -0,0 +1,17 @@
+//
+// Created by USER on 10/9/2023.
+//
+#include <stdio.h>
+int main()
+{
+    int n, c=0;
+    printf("Enter temperature: ");
+    scanf("%d",&n);
+
+    if(n>=100){
+    printf("Its Boiling!!");
+    }
+    else{
+        printf("Its Not Boiling");
+    }
+}
