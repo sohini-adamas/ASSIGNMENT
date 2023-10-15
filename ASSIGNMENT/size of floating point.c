d651060d36869eb6cc72cd93be9406b938f8b9fe
Sohini <145785887+sohini-adamas@users.noreply.github.com>
2023-10-16 00:10:06 +0530

    1st commit

---
+++ ASSIGNMENT/size of floating point.c
@@ -0,0 +1,10 @@
+//
+// Created by USER on 10/16/2023.
+//
+//Write a program in to print the size of a data type in floating point.
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    printf("%f",(float)sizeof(a));
+}
